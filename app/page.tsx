06ae64ea456a5a32ff7f78483f0b3347501500d9
萱島菜穂 <caisuiz839@gmail.com>
2026-06-01 23:32:07 +0900

    feat: Tailwindでトップページにスタイル追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
 export default function Home() {
   return (
-    <main>
-      <h1>こんにちは！</h1>
+    <main className="min-h-screen bg-gray-900 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">
+        こんにちは！
+      </h1>
     </main>
   )
 }
